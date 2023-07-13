fbd5369d9f9b9952f0e48bc9c1c185f7f85ccd63
Richard H <tevesek3@gmail.com>
2023-07-13 23:08:32 +0200

    Initilazie git repo, Add game file, Add external words.txt for game words collection to choose from

---
+++ README.md
@@ -0,0 +1,24 @@
+# Memory Game
+
+This is a simple memory game implemented in Python. The game tests your memory skills by challenging you to remember and recall words in a given order. Each level increases the number of words to remember, providing an escalating difficulty.
+
+## Features
+
+- Multiple levels with increasing difficulty.
+- Random selection of words from an external file.
+- Case-insensitive word comparison.
+- Clear instructions for user input format.
+- Display of correct answer when the user makes a mistake.
+- Tracking of the maximum level reached by the user.
+
+## Instructions
+
+1. Make sure you have Python 3.x installed on your system.
+2. Clone this repository to your local machine or download the ZIP file.
+3. Place a text file named `words.txt` in the same directory as the Python script.
+4. Add your desired words to the `words.txt` file, separating them by a comma and space (e.g., `Apple, Orange, Banana`).
+5. Open a terminal or command prompt and navigate to the directory where the script is located.
+6. Run the game by executing the following command:
+
+```bash
+python memory_game.py
---
+++ game.py
@@ -0,0 +1,56 @@
+import random
+
+def load_words(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+        words = [word.strip() for word in content.split(', ')]
+    return words
+
+def play_memory_game(words, level):
+    print(f"Level {level}:")
+    print("***************************")
+    print(f"Remember the word: {words[level-1]}")
+    print("***************************")
+    user_input = input("Enter the word(s) separated by comma and space: ")
+    user_words = [word.strip() for word in user_input.split(',')]
+    expected_words = words[:level]
+    
+    if len(user_words) != level or not all(word.lower() == expected_words[i].lower() for i, word in enumerate(user_words)):
+        print("Game Over! You made a mistake.")
+        print(f"The correct answer should have been: {', '.join(expected_words)}")
+        return False
+    
+    print("Congratulations! You passed the level.")
+    return True
+
+
+def main():
+    file_path = "words.txt"  # Path to the external txt file
+    words = load_words(file_path)
+    random.shuffle(words)
+
+    print(""" __ __   ___   __ __    __    ___   __   __    __    __    __ __   ___  
+|  V  | | __| |  V  |  /__\  | _ \  \ `v' /   / _]  /  \  |  V  | | __| 
+| \_/ | | _|  | \_/ | | \/ | | v /   `. .'   | [/\ | /\ | | \_/ | | _|  
+|_| |_| |___| |_| |_|  \__/  |_|_\    !_!     \__/ |_||_| |_| |_| |___| 
+
+""")
+    print("****************************")
+    print("Enter each word separated by comma and space (e.g., apple, orange, banana).")
+
+    max_level = 0
+    for level in range(1, len(words) + 1):
+        if play_memory_game(words, level):
+            print("****************************")
+            print("Level completed! Proceeding to the next level.")
+            max_level = level
+        else:
+            print("****************************")
+            print("Game Over! Try again.")
+            break
+    
+    print(f"Max level reached: {max_level}")
+
+if __name__ == '__main__':
+    main()
+
